d870f3a9fd96c9b5ff810e17034dd52ca1ce07e0
MNDIOM <mamadou.johm@gmail.com>
2024-07-11 18:23:11 -0400

    components added

---
+++ src/InfoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InfoPage = () => {
+    return (
+        <div>
+            <h1>Info Page</h1>
+            <p>Find out more information about us.</p>
+        </div>
+    );
+};
+
+export default InfoPage;
