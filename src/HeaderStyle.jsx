29391d98cad00240f14ad83b9b28c1914f0b6717
MNDIOM <mamadou.johm@gmail.com>
2024-07-13 13:05:18 -0400

    Hero image updated

--- src/HeaderStyle.jsx
+++ src/HeaderStyle.jsx
@@ -92,7 +92,7 @@ const Hero = () => {
               <div className="lg:ml-auto lg:text-right">
                 <div className="relative z-10 inline-block pt-11 lg:pt-0">
                   <img
-                    src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png"
+                    src="https://static01.nyt.com/images/2023/06/23/multimedia/23BIGIDEAS-SAMUELSSON-ktwp/23BIGIDEAS-SAMUELSSON-ktwp-mediumSquareAt3X.jpg"
                     alt="hero"
                     className="max-w-full lg:ml-auto"
                   />
