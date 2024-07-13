6621b9c5f2ef59242e75787cca57be61864d672c
mesfinhg <mesfinhgobena@gmail.com>
2024-07-13 12:59:30 -0400

    added menu date file and revamped portfolio page arhtecture

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
+import menuData from "./menuData";
 
 const Portfolio = () => {
-  const [showCard, setShowCard] = useState("all");
-
-  const handleProject = (category) => {
-    setShowCard(category);
-  };
+  const [categoryId, setCategoryId] = useState();
+  const [categoryName, setCategoryName] = useState("all");
 
   return (
     <>
@@ -14,15 +12,11 @@ const Portfolio = () => {
           <div className="flex flex-wrap -mx-4">
             <div className="w-full px-4">
               <div className="mx-auto mb-[60px] max-w-[510px] text-center">
-                <span className="text-primary mb-2 block text-lg font-semibold">
-                  
-                </span>
+
                 <h2 className="text-dark mb-3 text-3xl leading-[1.208] font-bold sm:text-4xl md:text-[40px]">
-                  Menu
+                  Our Menu
                 </h2>
-                <p className="text-body-color text-base dark:text-dark-6">
-                  Enjoy our selection of delicious exquisite Habesha food!
-                </p>
+
               </div>
             </div>
           </div>
@@ -30,118 +24,61 @@ const Portfolio = () => {
           <div className="w-full flex flex-wrap justify-center -mx-4">
             <div className="w-full px-4">
               <ul className="flex flex-wrap justify-center mb-12 space-x-1">
-                <li className="mb-1">
-                  <button
-                    onClick={() => handleProject("all")}
-                    className={`inline-block rounded-lg py-2 px-5 text-center text-base font-semibold transition md:py-3 lg:px-8 ${
-                      showCard === "all"
-                        ? "activeClasses bg-primary text-white"
-                        : "inactiveClasses text-body-color dark:text-dark-6 hover:bg-primary hover:text-white"
-                    }`}
-                  >
-                    Main Course
-                  </button>
-                </li>
-                <li className="mb-1">
-                  <button
-                    onClick={() => handleProject("branding")}
-                    className={`inline-block rounded-lg py-2 px-5 text-center text-base font-semibold transition md:py-3 lg:px-8 ${
-                      showCard === "branding"
-                        ? "activeClasses bg-primary text-white"
-                        : "inactiveClasses text-body-color dark:text-dark-6 hover:bg-primary hover:text-white"
-                    }`}
-                  >
-                    Branding
-                  </button>
-                </li>
-                <li className="mb-1">
-                  <button
-                    onClick={() => handleProject("design")}
-                    className={`inline-block rounded-lg py-2 px-5 text-center text-base font-semibold transition md:py-3 lg:px-8 ${
-                      showCard === "design"
-                        ? "activeClasses bg-primary text-white"
-                        : "inactiveClasses text-body-color dark:text-dark-6 hover:bg-primary hover:text-white"
-                    }`}
-                  >
-                    Design
-                  </button>
-                </li>
-                <li className="mb-1">
-                  <button
-                    onClick={() => handleProject("marketing")}
-                    className={`inline-block rounded-lg py-2 px-5 text-center text-base font-semibold transition md:py-3 lg:px-8 ${
-                      showCard === "marketing"
-                        ? "activeClasses bg-primary text-white"
-                        : "inactiveClasses text-body-color dark:text-dark-6 hover:bg-primary hover:text-white"
-                    }`}
-                  >
-                    Marketing
-                  </button>
-                </li>
-                <li className="mb-1">
-                  <button
-                    onClick={() => handleProject("development")}
-                    className={`inline-block rounded-lg py-2 px-5 text-center text-base font-semibold transition md:py-3 lg:px-8 ${
-                      showCard === "development"
-                        ? "activeClasses bg-primary text-white"
-                        : "inactiveClasses text-body-color dark:text-dark-6 hover:bg-primary hover:text-white"
-                    }`}
-                  >
-                    Development
-                  </button>
-                </li>
+                {
+                  menuData && menuData.map(menuDataItem => {
+                    return (<li key={menuDataItem.id} className="mb-1">
+                      <button
+                        key={menuDataItem.id - 1}
+                        onClick={(e) => {
+                          setCategoryId(menuDataItem.id)
+                          setCategoryName(menuDataItem.category)
+                        }}
+                        className={`inline-block rounded-lg py-2 px-5 text-center text-base font-semibold transition md:py-3 lg:px-8 ${categoryId === menuDataItem.id ? "activeClasses bg-primary text-white"
+                          : "inactiveClasses text-body-color dark:text-dark-6 hover:bg-primary hover:text-white"
+                          }`}
+                      >
+                        {menuDataItem.category}
+                      </button>
+                    </li>)
+                  })
+                }
               </ul>
             </div>
           </div>
           <div className="flex flex-wrap -mx-4">
-            <PortfolioCard
-              ImageHref="https://i.ibb.co/64WfFPt/image-01.jpg"
-              category="Branding"
-              title="Creative Agency"
-              button="View Details"
-              buttonHref="#"
-              showCard={showCard}
-            />
-            <PortfolioCard
-              ImageHref="https://i.ibb.co/PT7ghRs/image-06.jpg"
-              category="marketing"
-              title="Creative Agency"
-              button="View Details"
-              buttonHref="#"
-              showCard={showCard}
-            />
-            <PortfolioCard
-              ImageHref="https://i.ibb.co/vkt8C1P/image-02.jpg"
-              category="marketing"
-              title="Creative Agency"
-              button="View Details"
-              buttonHref="#"
-              showCard={showCard}
-            />
-            <PortfolioCard
-              ImageHref="https://i.ibb.co/3FKqS1G/image-03.jpg"
-              category="Development"
-              title="Creative Agency"
-              button="View Details"
-              buttonHref="#"
-              showCard={showCard}
-            />
-            <PortfolioCard
-              ImageHref="https://i.ibb.co/m6dq2fX/image-04.jpg"
-              category="Design"
-              title="Creative Agency"
-              button="View Details"
-              buttonHref="#"
-              showCard={showCard}
-            />
-            <PortfolioCard
-              ImageHref="https://i.ibb.co/mCPjBsH/image-05.jpg"
-              category="Marketing"
-              title="Creative Agency"
-              button="View Details"
-              buttonHref="#"
-              showCard={showCard}
-            />
+            {
+              categoryName === "all" ? menuData.map(menuDataItem => {
+                return (
+                  menuDataItem.items.map(menuItem => {
+                    return (
+                      <PortfolioCard
+                        key={menuItem.id}
+                        ImageHref="https://i.ibb.co/64WfFPt/image-01.jpg"
+                        category={menuDataItem.category}
+                        title={menuItem.name}
+                        price={menuItem.price}
+                        description={menuItem.desc}
+                      />
+                    )
+                  })
+                )
+              }) : menuData.filter(categoryObject => categoryObject.id === categoryId).map(menuDataItem => {
+                return (
+                  menuDataItem.items.map(menuItem => {
+                    return (
+                      <PortfolioCard
+                        key={menuItem.id}
+                        ImageHref="https://i.ibb.co/64WfFPt/image-01.jpg"
+                        category={menuDataItem.category}
+                        title={menuItem.name}
+                        price={menuItem.price}
+                        description={menuItem.desc}
+                      />
+                    )
+                  })
+                )
+              })
+            }
           </div>
         </div>
       </section>
@@ -152,37 +89,25 @@ const Portfolio = () => {
 export default Portfolio;
 
 const PortfolioCard = ({
-  showCard,
   category,
   ImageHref,
   title,
-  button,
-  buttonHref,
+  price,
+  description
 }) => {
   return (
     <>
       <div
-        className={`w-full px-4 md:w-1/2 xl:w-1/3 ${
-          showCard === "all" || showCard === category.toLowerCase()
-            ? "block"
-            : "hidden"
-        }`}
+        className={"w-full px-4 md:w-1/2 xl:w-1/3"}
       >
         <div className="relative mb-12">
           <div className="overflow-hidden rounded-[10px]">
             <img src={ImageHref} alt="portfolio" className="w-full" />
           </div>
           <div className="relative z-10 mx-7 -mt-20 rounded-lg bg-white dark:bg-dark-2 py-[34px] px-3 text-center shadow-portfolio dark:shadow-box-dark">
-            <span className="text-primary mb-2 block text-sm font-medium">
-              {category}
-            </span>
             <h3 className="text-dark dark:text-white mb-5 text-xl font-bold">{title}</h3>
-            <a
-              href={buttonHref}
-              className="text-body-color dark:text-dark-6 hover:border-primary hover:bg-primary inline-block rounded-md border border-stroke dark:border-dark-3 py-[10px] px-7 text-sm font-medium transition hover:text-white"
-            >
-              {button}
-            </a>
+            <h4 className="text-dark dark:text-white mb-5 text-xl font-bold">{price}</h4>
+            <h4 className="text-dark dark:text-white mb-5 text-xl font-bold">{description}</h4>
           </div>
         </div>
       </div>
