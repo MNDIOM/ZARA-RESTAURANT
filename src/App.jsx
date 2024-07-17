2b4a592d6661684348bd783133ce9de1b062b0a9
nurgessa <168490517+nurgessa@users.noreply.github.com>
2024-07-16 20:49:51 -0400

    updated footer, removed tailgrid logo, added contact, remove carrer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
 import { useState } from 'react';
 import './App.css';
 import Portfolio from'./Portfolio';
-<<<<<<< HEAD
-// import Stats from'./Stats';
-import Testimonials from'./Testimonials.jsx';
-
-=======
 import HeaderStyle from './HeaderStyle';
 import Footer from './Footer';
 import Testimonials from './Testimonials';
 import About from './About'
->>>>>>> d9cef5acdecd81ed7e3479d5950099535c47201a
 
 function App() {
     const [count, setCount] = useState(0);
  return (
    <>
-<<<<<<< HEAD
-   <div>
-      <HeaderStyle/>
-      <Portfolio/>
-      <Testimonials/>
-      <Footer/>
-    </div>
-=======
    <HeaderStyle/>
     <Portfolio/>
     <About/>
     <Testimonials/>
     <Footer/>
->>>>>>> d9cef5acdecd81ed7e3479d5950099535c47201a
     </>
  )       
    
