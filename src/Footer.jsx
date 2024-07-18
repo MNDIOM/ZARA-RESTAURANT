ce01223872901dcb3288c1f78616380960d735ab
nurgessa <168490517+nurgessa@users.noreply.github.com>
2024-07-18 18:35:06 -0400

    netsi latest change

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -109,33 +109,6 @@ const Footer = () => {
               </defs>
             </svg>
           </span>
-          <span className="absolute right-10 top-10 z-[-1]">
-            <svg
-              width={75}
-              height={75}
-              viewBox="0 0 75 75"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M37.5 -1.63918e-06C58.2107 -2.54447e-06 75 16.7893 75 37.5C75 58.2107 58.2107 75 37.5 75C16.7893 75 -7.33885e-07 58.2107 -1.63918e-06 37.5C-2.54447e-06 16.7893 16.7893 -7.33885e-07 37.5 -1.63918e-06Z"
-                fill="url(#paint0_linear_1179_4)"
-              />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_1179_4"
-                  x1="-1.63917e-06"
-                  y1="37.5"
-                  x2={75}
-                  y2="37.5"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#13C296" stopOpacity="0.31" />
-                  <stop offset={1} stopColor="#C4C4C4" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-            </svg>
-          </span>
         </div>
       </footer>
     </>
